de3c90305d0111430c53105ac85a2ba916dbe421
Boudriga <boudrigaahmed7@gmail.com>
2024-03-28 13:12:00 +0100

    Initial version of pfe generated by generator-jhipster@8.1.0

--- src/main/webapp/app/layouts/sidebar/sidebar.component.ts
+++ src/main/webapp/app/layouts/sidebar/sidebar.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterLinkActive} from "@angular/router";
+import {Router, RouterLink, RouterLinkActive} from "@angular/router";
 import HasAnyAuthorityDirective from "../../shared/auth/has-any-authority.directive";
+import {VERSION} from "../../app.constants";
+import {LoginService} from "../../login/login.service";
 
 @Component({
   selector: 'reclamation-sidebar',
@@ -14,5 +16,17 @@ import HasAnyAuthorityDirective from "../../shared/auth/has-any-authority.direct
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
-
+  version = '';
+  constructor(
+    private loginService: LoginService,
+    private router: Router,
+  ){
+    if (VERSION) {
+      this.version = VERSION.toLowerCase().startsWith('v') ? VERSION : `v${VERSION}`;
+    }
+  }
+  logout(): void {
+    this.loginService.logout();
+    this.router.navigate(['']);
+  }
 }
