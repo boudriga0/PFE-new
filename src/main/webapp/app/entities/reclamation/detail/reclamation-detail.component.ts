97a3e403425b833ebd46bb4914b8befe765207f8
Boudriga <boudrigaahmed7@gmail.com>
2024-04-15 09:40:28 +0100

    Initial version of pfe generated by generator-jhipster@8.1.0

--- src/main/webapp/app/entities/reclamation/detail/reclamation-detail.component.ts
+++ src/main/webapp/app/entities/reclamation/detail/reclamation-detail.component.ts
@@ -1,11 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
-
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 import SharedModule from 'app/shared/shared.module';
 import { DurationPipe, FormatMediumDatetimePipe, FormatMediumDatePipe } from 'app/shared/date';
 import { IReclamation } from '../reclamation.model';
 import HasAnyAuthorityDirective from "../../../shared/auth/has-any-authority.directive";
-
+import {findIconDefinition} from "@fortawesome/fontawesome-svg-core";
+import {createPdf} from "pdfmake/build/pdfmake";
+(pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
 @Component({
   standalone: true,
   selector: 'reclamation-reclamation-detail',
@@ -20,5 +23,20 @@ export class ReclamationDetailComponent {
   previousState(): void {
     window.history.back();
   }
+  generatePDF(){
+    let docDefinition = {
+      content:[
+        { text: 'Réclamation:', style: 'header' },
+        { text: `ID: ${this.reclamation?.categorie}`, style: 'subheader' },
+        { text: `Nom du projet: ${this.reclamation?.categorie}`, style: 'subheader' },
+        { text: `Etat: ${this.reclamation?.etat}`, style: 'subheader' },
+        { text: `Plainte: ${this.reclamation?.numero}`, style: 'subheader' },
+      ],
+
+    };
+    pdfMake.createPdf(docDefinition).open();
+  }
+
+
 
 }
